d92335beeab66a0851f34b1730f8c20eaa682bec
ShreyaSingh10 <shreyasssingh10@gmail.com>
2018-04-03 15:04:36 +0530

    Second commit - Added two components- App.js and Popular.js

---
+++ app/components/Popular.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Popular extends React.Component {
+	render() {
+		return (
+			<div>
+				I am miss Popular!
+			</div>
+		)
+	}
+}
+
+module.exports = Popular;
